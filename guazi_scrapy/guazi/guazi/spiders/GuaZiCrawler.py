9e54a0b3c2c6571266476be816be78062f28523d
wangqing <wangqing@123>
2018-10-08 12:55:53 +0800

    1. 在md文件中添加model 到表创建逻辑 2. 天机pieple ，将数据保存到数据库中

--- guazi_scrapy/guazi/guazi/spiders/GuaZiCrawler.py
+++ guazi_scrapy/guazi/guazi/spiders/GuaZiCrawler.py
@@ -26,6 +26,7 @@ class AppleCrawler(scrapy.Spider):
     start_urls = ['https://www.guazi.com/cq/buy']
     userAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
     cookie = ''
+
     # custom_settings = {'DOWNLOAD_DELAY': 2}
     #
     # 新加的代码
@@ -75,25 +76,25 @@ class AppleCrawler(scrapy.Spider):
         self.commonSet(item, re.sub(r'\r\n|\s', '', owner), 'carOwner',
                        r'车主：\s*(.+)\s*')
         for liItem in res.select('.basic-eleven li'):
-            self.commonSet(liItem, re.sub(r'\r\n|\s', '', liItem.text), 'drivingDistance',
+            self.commonSet(item, re.sub(r'\r\n|\s', '', liItem.text), 'drivingDistance',
                            r'\s*(.+)表显里程\s*')
-            self.commonSet(liItem, re.sub(r'\r\n|\s', '', liItem.text), 'filingAddress',
+            self.commonSet(item, re.sub(r'\r\n|\s', '', liItem.text), 'filingAddress',
                            r'\s*(.+)上牌地\s*')
-            self.commonSet(liItem, re.sub(r'\r\n|\s', '', liItem.text), 'lastTime',
+            self.commonSet(item, re.sub(r'\r\n|\s', '', liItem.text), 'lastTime',
                            r'\s*(.+)上牌时间\s*')
-            self.commonSet(liItem, re.sub(r'\r\n|\s', '', liItem.text), 'transmissionCase',
+            self.commonSet(item, re.sub(r'\r\n|\s', '', liItem.text), 'transmissionCase',
                            r'\s*(.+)变速箱\s*')
-            self.commonSet(liItem, re.sub(r'\r\n|\s', '', liItem.text), 'displacement',
-                           r'\s*(.+)排量(L)\s*')
-            self.commonSet(liItem, re.sub(r'\r\n|\s', '', liItem.text), 'numberOfTransfer',
+            self.commonSet(item, re.sub(r'\r\n|\s', '', liItem.text), 'displacement',
+                           r'\s*(.+)排量\s*')
+            self.commonSet(item, re.sub(r'\r\n|\s', '', liItem.text), 'numberOfTransfer',
                            r'\s*(.+)登记证为准\s*')
-            self.commonSet(liItem, re.sub(r'\r\n|\s', '', liItem.text), 'carAddress',
+            self.commonSet(item, re.sub(r'\r\n|\s', '', liItem.text), 'carAddress',
                            r'\s*(.+)看车地址\s*')
-            self.commonSet(liItem, re.sub(r'\r\n|\s', '', liItem.text), 'yearlyInspection',
+            self.commonSet(item, re.sub(r'\r\n|\s', '', liItem.text), 'yearlyInspection',
                            r'\s*(.+)年检到期\s*')
-            self.commonSet(liItem, re.sub(r'\r\n|\s', '', liItem.text), 'trongInsurances',
+            self.commonSet(item, re.sub(r'\r\n|\s', '', liItem.text), 'trongInsurances',
                            r'\s*(.+)交强险\s*')
-            self.commonSet(liItem, re.sub(r'\r\n|\s', '', liItem.text), 'commercialInsurance',
+            self.commonSet(item, re.sub(r'\r\n|\s', '', liItem.text), 'commercialInsurance',
                            r'\s*(.+)商业险到期\s*')
 
         tables = res.select('.detailcontent table')
@@ -101,21 +102,21 @@ class AppleCrawler(scrapy.Spider):
         for trItem in tables[0].select('tr'):
             self.commonSet(item, re.sub(r'\r\n|\s', '', trItem.text), 'standard_type',
                            r'证件品牌型号此信息为机动车行驶证上的品牌型号信息，品牌型号信息以此为准\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'standard_producer', r'生产商\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'standard_producer', r'厂商\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'standard_grade', r'级别\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'standard_motor', r'发动机\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'standard_gearbox', r'变速箱\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'standard_structure', r'车生结构\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'standard_volume', r'长*宽*高\(mm\)\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'standard_structure', r'车身结构\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'standard_volume', r'长\*宽\*高\(mm\)\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'standard_wheelbase', r'轴距\(mm\)\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'standard_cargoVolume', r'行李箱容积\(L\)\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'standard_quality', r'装备质量\(kg\)\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'standard_quality', r'整备质量\(kg\)\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'standard_grade', r'级别\s*(.+)\s*')
 
         # 发动机参数
         for trItem in tables[1].select('tr'):
             self.commonSet(item, re.sub(r'\r\n|\s', '', trItem.text), 'motor_displacement',
-                           r'排量(L)\s*(.+)\s*')
+                           r'排量\(L\)\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'motor_intakeForm', r'进气形式\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'motor_cylinder', r'气缸\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'motor_maxPs', r'最大马力\(Ps\)\s*(.+)\s*')
@@ -130,12 +131,12 @@ class AppleCrawler(scrapy.Spider):
         for trItem in tables[2].select('tr'):
             self.commonSet(item, trItem.text, 'chassis_drivingMode', r'驱动方式\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'chassis_typeOfSupport', r'助力类型\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'chassis_frontSuspension', r'前悬挂类型\(Ps\)\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'chassis_rearSuspension', r'后悬挂类型\(N\*m\)\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'chassis_frontSuspension', r'前悬挂类型\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'chassis_rearSuspension', r'后悬挂类型\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'chassis_frontBrake', r'前制动类型\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'chassis_rearBrake', r'后制动类型\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'chassis_parkingBrakeType', r'驻车制动类型\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'chassis_parkingBrakeType', r'前轮胎规格\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'chassis_frontTireSpecification', r'前轮胎规格\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'chassis_rearTireSpecification', r'后轮胎规格\s*(.+)\s*')
 
         ####################  安全配置   ###################
@@ -143,38 +144,37 @@ class AppleCrawler(scrapy.Spider):
         for trItem in tables[3].select('tr'):
             self.commonSet(item, trItem.text, 'security_principalAndAssistant', r'主副驾驶安全气囊\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'security_roundSide', r'前后排侧气囊\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'security_roundHead', r'前后排头部气囊\(Ps\)\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'security_tirePressureDetection', r'胎压检测\(N\*m\)\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'security_roundHead', r'前后排头部气囊\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'security_tirePressureDetection', r'胎压检测\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'security_internalControlLock', r'车内中控锁\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'security_childSeat', r'儿童座椅接口\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'security_keylessStartUp', r'无钥匙启动\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'security_antiLockBrakingSystem', r'防抱死系统\(ABS\)\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'security_vehicleStabilityControlSystem', r'车身稳定控制\(ESP\)\s*(.+)\s*')
 
-
         ####### 外部配置 ##########
         for trItem in tables[4].select('tr'):
             self.commonSet(item, trItem.text, 'outer_electricSkylight', r'电动天窗\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'outer_panoramicSunroof', r'全景天窗\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'outer_electricallyOperatedSuctionDoor', r'电动吸合门\(Ps\)\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'outer_speechTrunk', r'感应后备箱\(N\*m\)\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'outer_electricallyOperatedSuctionDoor', r'电动吸合门\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'outer_speechTrunk', r'感应后备箱\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'outer_inductionWiper', r'感应雨刷\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'outer_rearWiper', r'后雨刷\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'security_keylessStartUp', r'前后电动车窗\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'security_antiLockBrakingSystem', r'后视镜电动调节\(ABS\)\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'security_vehicleStabilityControlSystem', r'后视镜加热\(ESP\)\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'outer_frontAndRearPowerWindows', r'前后电动车窗\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'outer_electricAdjustmentOfRearviewMirror', r'后视镜电动调节\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'outer_rearviewMirrorHeating', r'后视镜加热\s*(.+)\s*')
 
         ########## 内部配置   #######
         for trItem in tables[5].select('tr'):
             self.commonSet(item, trItem.text, 'internal_multifunctionalSteeringWheel', r'多功能方向盘\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'internal_cruiseControl', r'定速巡航\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'internal_airConditioner', r'空调\(Ps\)\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'internal_automaticAirConditioning', r'自动空调\(N\*m\)\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'internal_airConditioner', r'空调\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'internal_automaticAirConditioning', r'自动空调\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'internal_gps', r'GPS导航\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'internal_PDCParkingDistanceControl', r'倒车雷达\s*(.+)\s*')
             self.commonSet(item, trItem.text, 'internal_rearCameraParkingAid', r'倒车影像系统\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'internal_leatherSeat', r'真皮座椅\(ABS\)\s*(.+)\s*')
-            self.commonSet(item, trItem.text, 'internal_frontAndRearSeatHeating', r'前后排座椅加热\(ESP\)\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'internal_leatherSeat', r'真皮座椅\s*(.+)\s*')
+            self.commonSet(item, trItem.text, 'internal_frontAndRearSeatHeating', r'前后排座椅加热\s*(.+)\s*')
 
         print(item)
         return item
